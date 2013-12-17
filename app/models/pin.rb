73428767b9a716baf21ee29e94b2d03ca27858ca
corysmith11 <csmith@rycoleinvest.com>
2013-12-16 23:24:40 -0700

    Load Pins to reverse chronological order

--- app/models/pin.rb
+++ app/models/pin.rb
@@ -1,4 +1,4 @@
 class Pin < ActiveRecord::Base
      belongs_to :user
-     has_attached_file :image, :styles => { :medium => "320x240>", :thumb => "100x100>" }
+     has_attached_file :image, :styles => { :medium => "1136x640>", :thumb => "100x100>" }
 end
